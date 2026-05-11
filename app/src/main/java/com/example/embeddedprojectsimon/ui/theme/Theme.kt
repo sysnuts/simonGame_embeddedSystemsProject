1b05d63357d539ff3a6e08e5dcfe00243289d818
matteoz <zanon.matteo@pm.me>
2026-05-11 19:41:42 +0200

    Aggiornate dipendenze e rimosso codice inutilizzato

--- app/src/main/java/com/example/embeddedprojectsimon/ui/theme/Theme.kt
+++ app/src/main/java/com/example/embeddedprojectsimon/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.embeddedprojectsimon.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
